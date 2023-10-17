500b3440ca46e45272c2a4a3babe2f7f4023dd4e
Anes Fehric (SJJ) <anes.fehric@bitconex.de>
2023-10-17 09:30:26 +0200

    second commit

--- src/main/java/com/filupload/h2database/Implementation/RuleApiImpl.java
+++ src/main/java/com/filupload/h2database/Implementation/RuleApiImpl.java
@@ -13,8 +13,12 @@ import org.apache.tomcat.util.digester.Rule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -45,6 +49,9 @@ public class RuleApiImpl implements RuleApi {
         File fileTest = new File();
 
         try {
+            test.setName(file.getName());
+            test.setContentType(file.getContentType());
+            test.setContentType(file.getContentType());
             fileTest.setName(file.getOriginalFilename());
             fileTest.setFileData(FileUtils.compressFile(file.getBytes()));
             fileTest.setType(file.getContentType());
@@ -59,8 +66,28 @@ public class RuleApiImpl implements RuleApi {
     }
 
     @Override
-    public ResponseEntity<Resource> ruleEngineFileDownload(Integer fileId) {
+    public ResponseEntity<Resource> ruleEngineFileDownload(Long fileId) {
         Optional<File> dbFileData = fileDao.findById(fileId);
 
+        if (dbFileData.isPresent()) {
+            byte[] fileBytes = FileUtils.decompressFile(dbFileData.get().getFileData());
+
+            // Wrap the file bytes in a Resource
+            Resource resource = new ByteArrayResource(fileBytes);
+
+            // Set the content type and disposition headers for the response
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + dbFileData.get().getName());
+
+            // Return the ResponseEntity with the resource and headers
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(fileBytes.length)
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } else {
+            // Handle the case where the file is not found
+            return ResponseEntity.notFound().build();
+        }
     }
 }
